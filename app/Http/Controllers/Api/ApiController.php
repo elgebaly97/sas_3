e56d5d1916903bc50581c4459953deacb6f8848f
elgebaly97 <helgebaly65@gmail.com>
2019-12-13 15:25:21 +0200

    commit edit

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -40,8 +40,8 @@ class ApiController extends Controller
         //$subjects = Subject::all();
         //return $this->apiResponse(1, '', $subjects);
         $subjects = Subject::where(function($query) use($request){
-            if($request->has('grade_id')){
-                $query->where('grade_id', $request->grade_id);
+            if($request->has('grade_id') and $request->has('department_id')){
+                $query->where('grade_id', $request->grade_id)->wehere('department_id', $request->department_id);
             }
         })->get();
         return $this->apiResponse(1, '', $subjects);
