fd793d4df8e5343a255afbcaca89ab813348d163
elgebaly97 <helgebaly65@gmail.com>
2020-01-12 13:51:35 +0200

    welcome api2

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,17 @@
 |
 */
 
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 
 Route::prefix('admin')->group(function () {
